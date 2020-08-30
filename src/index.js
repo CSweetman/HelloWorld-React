ddc27aa251c04d7fb980da6be2607f5aa7f7a226
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-08-30 12:57:39 -0500

    Created Hello World application Changes: Learned how to create a hello world applications which actually taught a lot more than other hello world applications. I learned the reason why certain modules and objects had to be imported, how to write html in js and render it into the virtual dom in react while also getting a small idea on how components will work in the future
    Security Concerns:
    N/A

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from 'react';  //'react is the module and React is object being imported from module
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Babel will compile this into React.createElement -> why we import React object since though we don't use it, it will be compiled into React.createLElement
+const element = <h1>Hello World</h1>;   //Virtual DOM is lightweight replicate, object is apart of it. Will update DOM accordingly.
+ReactDOM.render(element, document.querySelector('#root'));  //Renders this in real DOM, why we import react-dom. 
+//^First argument is element to render, second is where to render it. Root is div container where the application is
+console.log(element);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+*In real application, won't render single elements but instead app components.
+*Lead to tree of componenets that lead up to complicated markup.
+*/
+
+//Now time to make shopping cart.
